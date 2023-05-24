ed5d98e0cbc72e3c4c166fd4cc24605580d7bd1d
ANISH MANDAL <mdlanish8848@gmail.com>
2023-05-24 10:55:00 +0545

    dfadf

--- college/1_practice/4_totalmark.cpp
+++ college/1_practice/4_totalmark.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
 using namespace std;
+#include<string>
 
 
 int main() {
      int subject = 5;
     int marks[subject];
     int total = 0;
+    int i;
 
 
     for (int i = 0; i < 5 ; ++i) {
-        std::cout << "Enter marks for subject " << i + 1 << ": ";
-        std::cin >> marks[i];
+        cout << "Enter marks for subject "<<i << ": ";
+        cin >> marks[i];
         total += marks[i];
     }
 
-    std::cout << "Total marks: " << total << std::endl;
+    cout << "Total marks: " << total << endl;
 
     return 0;
 }
